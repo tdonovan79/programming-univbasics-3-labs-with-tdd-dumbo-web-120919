edfd62bae9284ca796616a69029a8bffc151e572
Thomas Donovan <tdonovan79@gmail.com>
2019-10-30 20:36:08 +0000

    Automatically backed up by Learn

---
+++ calculator.rb
@@ -0,0 +1,6 @@
+first_number = 2
+second_number = 0
+sum = first_number + second_number
+difference = first_number - second_number
+product = first_number * second_number
+quotient = first_number / second_number
